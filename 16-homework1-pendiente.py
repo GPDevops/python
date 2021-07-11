30b82247653ffab18d5f740e43fffe80be06da74
GPonce <soporte.igrator@gmail.com>
2021-07-10 21:20:13 -0500

    version2

--- 16-homework1-pendiente.py
+++ 16-homework1-pendiente.py
@@ -1,10 +1,7 @@
 # homework: imprimir los primeros 10 numeros primos 
 ## eg, 2,3,5,7
-
 #numero primo = divisible por si mismo y para la unidad unicamente
-
-
-       
+      
 numberofprimes =0
 number =2
 while numberofprimes < 10:
