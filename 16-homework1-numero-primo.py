efaac546439be7a9495c3f8e9670fa936eb9af7f
gchalo17 <gchalo17@gmail.com>
2022-10-11 20:43:54 -0500

    sin cambios

--- 16-homework1-numero-primo.py
+++ 16-homework1-numero-primo.py
@@ -7,7 +7,7 @@ number =2
 while numberofprimes < 10:
     isprime =True
     for i in range(2, number):
-        if number % i ==0:
+        if number % i == 0:
              isprime =False
     if isprime == True:
         print(number, end=" ")
