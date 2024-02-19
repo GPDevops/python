73243df3c70435c50e1e904c41eceeda24d96d7e
GPDEVOPS <gchalo17@gmail.com>
2024-02-19 14:13:24 -0500

    change

--- 7-ingreso_datos.py
+++ 7-ingreso_datos.py
@@ -2,7 +2,7 @@
 palabra = input("Introduce una palabra: ")
 numero_int = int(input("Introduce un numero entero: "))
 numero_float = float(input("Introduce un numero flotante: "))
-numero_complejo = complex(input("Introduce un numero complejo: ")) #formato 2+3j
+numero_complejo = complex(input("Introduce un numero complejo en formato por ejemplo=2+3j: ")) #formato 2+3j
 
 print("String: ", palabra)
 print("Entero: ", numero_int)
