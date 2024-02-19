67e9cb4914b6091a9cd9997990dd091d930e70d2
GPDEVOPS <gchalo17@gmail.com>
2024-02-19 12:59:57 -0500

    change

--- 4-concatenacion.py
+++ 4-concatenacion.py
@@ -14,4 +14,10 @@ print("El resultado usando concatenacion es: " + resultado)
 
 ################################
 ##course web developer course
-
+print("####################")
+print('Suma de numeros enteros con opcion de concatenacion')
+valor1=int(input('Ingrese numero1: '))
+valor2=int(input('Ingrese numero2: '))
+resultado = str(valor1 + valor2)
+print("El resultado de la suma es: " + resultado)
+      
